9e42a39345542b10fa000414bf831b4fdc9d3a90
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-01-22 18:28:21 +0900

    feat: 글쓰기 페이지 및 컨트롤러 구현

---
+++ spring-mvc/board/src/main/java/basic/board/controller/BoardController.java
@@ -0,0 +1,21 @@
+package basic.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/board")
+public class BoardController {
+    @GetMapping("/save")
+    public String saveForm() {
+        return "/board/save";
+    }
+
+    @PostMapping("/save")
+    public String save() {
+        return null;
+    }
+}
